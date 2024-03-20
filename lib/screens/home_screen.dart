113a643bf746be49ef9325d45138e244b5cc5095
Jakub Czarski <jakub.czarski@gmail.com>
2024-03-20 07:08:51 +0100

    init commit, few buttons added, few styles

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,70 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+import 'package:flutterporject/utils/app_style.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 223, 214, 214),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                _mainButton(() => null, "Nowy plik"),
+                Row(
+                  children: [
+                    _actionButton(() => null, Icons.file_upload),
+                    _actionButton(() => null, Icons.folder),
+                  ],
+                )
+              ],
+            ),
+            const SizedBox(height: 20),
+            TextField(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  ElevatedButton _mainButton(Function()? onPress, String text) {
+    return ElevatedButton(
+      onPressed: onPress,
+      child: Text(text),
+      style: _buttonStyle(),
+    );
+  }
+
+  IconButton _actionButton(Function()? onPress, IconData icon) {
+    return IconButton(
+      onPressed: onPress,
+      splashRadius: 20,
+      splashColor: AppTheme.accent,
+      icon: Icon(
+        icon,
+        color: AppTheme.medium,
+      ),
+    );
+  }
+
+  ButtonStyle _buttonStyle() {
+    return ElevatedButton.styleFrom(
+        backgroundColor: AppTheme.accent,
+        foregroundColor: AppTheme.dark,
+        disabledBackgroundColor: AppTheme.disabledBackgroundColor,
+        disabledForegroundColor: AppTheme.disabledForegroundColor);
+  }
+}
